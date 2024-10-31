fe3a2ca5192b7234ce0403f8c067ed8728283cb9
Vatsal Maheshwari <f20180855@pilani.bits-pilani.ac.in>
2024-10-31 23:32:10 +0530

    Modified the name of the Type of Design Pattern from Structural to Creational

---
+++ Creational Design Pattern/Factory Pattern/Factory Pattern/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory obj = new ShapeFactory();
+        Shape shapeObj = obj.getShape("CIRCLE");
+        shapeObj.draw();
+        shapeObj = obj.getShape("RECTANGLE");
+        shapeObj.draw();
+    }
+}
